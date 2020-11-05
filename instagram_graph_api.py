2683391031cf00176a482402ea1423b31596b898
rplusq <rafaplusq@gmail.com>
2020-11-05 16:37:51 -0400

    mejoras a la herramienta, nuevas métricas

--- instagram_graph_api.py
+++ instagram_graph_api.py
@@ -172,8 +172,28 @@ def getUserInsights( params ) :
     """
 
     endpointParams = dict() # parameter to send to the endpoint
-    endpointParams['metric'] = 'follower_count,impressions,profile_views,reach' # fields to get back
+    endpointParams['metric'] = 'follower_count,impressions,profile_views,reach,email_contacts,get_directions_clicks,phone_call_clicks,text_message_clicks,website_clicks' # fields to get back
     endpointParams['period'] = 'day' # period
+    endpointParams['access_token'] = params['access_token'] # access token
+
+    url = params['endpoint_base'] + params['instagram_account_id'] + '/insights' # endpoint url
+
+    return makeApiCall( url, endpointParams, params['debug'] ) # make the api call
+
+def getAudienceInsights( params ):
+    """ Get insights for a users account audience
+    
+    API Endpoint:
+        https://graph.facebook.com/{graph-api-version}/{ig-user-id}/insights?metric={metric}&period={period}
+
+    Returns:
+        object: data from the endpoint
+
+    """
+
+    endpointParams = dict() # parameter to send to the endpoint
+    endpointParams['metric'] = 'online_followers,audience_city,audience_country,audience_gender_age,audience_locale' # fields to get back
+    endpointParams['period'] = 'lifetime' # period
     endpointParams['access_token'] = params['access_token'] # access token
 
     url = params['endpoint_base'] + params['instagram_account_id'] + '/insights' # endpoint url
